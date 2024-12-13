f3730df6515ebaa9bfe9e169af8f696dfe85906a
van-du-2005 <dun3632@gmail.com>
2024-12-13 21:52:17 +0700

    complete QuanLyBan

--- LTWINDOW_/QuanLiBan.cs
+++ LTWINDOW_/QuanLiBan.cs
@@ -19,10 +19,7 @@ namespace LTWINDOW_
             InitializeComponent();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
+        
 
         void loadData()
         {
@@ -147,6 +144,91 @@ namespace LTWINDOW_
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // khởi tạo form thêm bàng.
+            AddBan addBan = new AddBan();
+
+            // thực thi xong form thêm bàn mới chạy câu lệnh tiếp theo.
+            addBan.ShowDialog();
+
+            // load lại datagrid view.
+            loadData();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            SuaBan suaBan = new SuaBan();
+            suaBan.ShowDialog();
+            loadData();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                if (MessageBox.Show("Bạn có chác muốn xóa những hàng đã chọn không?", "Xóa Hàng Đã Chọn", MessageBoxButtons.YesNo) == DialogResult.No)
+                    return;
+
+                // câu truy vấn.
+                string query = "delete from Ban where MaBan = @MaBan";
+
+                SQL sql = new SQL();
+
+
+                int check = 0; // kiểm tra xem có xóa được dữ liệu chưa.
+
+                using (SqlConnection connection = sql.Conn)
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        SqlCommand command = new SqlCommand(query, connection);
+
+                        while (dataGridView1.SelectedRows.Count > 0)
+                        {
+                            DataGridViewRow row = dataGridView1.SelectedRows[0];
+
+                            if (row.IsNewRow) continue;
+
+                            var getValueColumnCurrent = dataGridView1.SelectedRows[0].Cells[0].Value;
+                            int MaBan = int.Parse(getValueColumnCurrent.ToString());
+
+                            // add parameter
+                            command.Parameters.AddWithValue("@MaBan", MaBan);
+
+                            // thực thi không tri vấn
+                            command.ExecuteNonQuery();
+
+                            command.Parameters.Clear();
+
+                            // xóa hàng đầu tiên được chọn.
+                            dataGridView1.Rows.Remove(row);
+                        }
+
+
+                        // dã xóa dữ liệu.
+                        check = 1;
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+
+                }
+
+
+                if (check == 1) loadData();  // nếu đã xóa thì load lại datagridView.
+            }
+            else MessageBox.Show("không có dòng nào để xóa hoặc Bạn chưa chọn dòng cần xóa");
         }
     }
 }
